94873c8ae0b9171af56e42ce594f2280fdcd6a09
SalomeTabaresJllo <salome.tabares@upb.edu.co>
2026-09-03 11:30:02 -0500

    Add files via upload

---
+++ 13. CICLOFOR/13. CICLOFOR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.CICLOFOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //CICLO FOR
+            int acumulador = 0;
+
+            for (int contador = 1; contador <=5; contador++)  //EJEMPLO HASTA EL 5
+            {
+                acumulador += contador;
+            }
+
+            Console.WriteLine("LA SUMA DE LOS PRIMEROS 5 NUMEROS ES" + acumulador);
+        }
+    }
+}
